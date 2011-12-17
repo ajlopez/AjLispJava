d108139913b375b493a5174a5767c16f01d59073
ajlopez <webmaster@ajlopez.com>
2011-12-17 16:07:34 -0300

    Bind with initial test

---
+++ src/main/java/com/ajlopez/ajlisp/forms/Bind.java
@@ -0,0 +1,29 @@
+package com.ajlopez.ajlisp.forms;
+
+import com.ajlopez.ajlisp.Environment;
+import com.ajlopez.ajlisp.IForm;
+import com.ajlopez.ajlisp.List;
+import com.ajlopez.ajlisp.primitives.Do;
+
+public class Bind implements IForm {
+	private static Do doprim = new Do();
+	private Object names;
+	private List body;
+	
+	public Bind(Object names, List body) {
+		this.names = names;
+		this.body = body;
+	}
+
+	public Object evaluate(Environment environment, List arguments) {
+		Environment newenv = environment;
+		
+		if (names != null) {
+			newenv = new Environment(environment);
+			newenv.setValues(this.names, arguments);
+		}
+		
+		return doprim.evaluate(newenv, this.body);
+	}
+
+}
---
+++ src/test/java/com/ajlopez/ajlisp/forms/BindTests.java
@@ -0,0 +1,34 @@
+package com.ajlopez.ajlisp.forms;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.ajlopez.ajlisp.Environment;
+import com.ajlopez.ajlisp.List;
+import com.ajlopez.ajlisp.Machine;
+import com.ajlopez.ajlisp.parser.LexerException;
+import com.ajlopez.ajlisp.parser.ParseException;
+import com.ajlopez.ajlisp.parser.Parser;
+import com.ajlopez.ajlisp.primitives.Cons;
+
+public class BindTests {
+
+	@Test
+	public void simpleEvaluate() throws IOException, ParseException, LexerException {
+		Object names = (new Parser("(a b)")).parseExpression();
+		List body = (List)(new Parser("((cons a b))")).parseExpression();
+		Bind bind = new Bind(names, body);
+		List arguments = (List)(new Parser("(1 2)")).parseExpression();
+
+		Environment environment = new Environment();
+		environment.setValue("cons", new Cons());
+		
+		Object result = bind.evaluate(environment, arguments);
+		
+		assertEquals("(1 . 2)", Machine.printString(result));
+	}
+
+}
